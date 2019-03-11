57d5f9a75a305517cee5c07e6ad20f7a73f6949e
Extibax <extibax@gmail.com>
2019-03-10 19:20:59 -0500

    first-angular-app

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  users = ['Juan', 'Sebastian', 'Leon', 'John', 'Bruce'];
+  activated: boolean = true;
+  name: string = 'Ryan Ray';
 }
