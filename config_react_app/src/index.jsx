c1167147d93831185c902075d3b9df962e718fc3
Extibax <extibax@gmail.com>
2019-03-18 20:06:36 -0500

    Config react app

---
+++ config_react_app/src/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Prueba extends Component {
+    render() {
+        return (
+            <h1>Hola Mundo</h1>
+        )
+    }
+}
+
+render (
+    <Prueba/>
+    document.getElementById('app');
+);
