68ba6c7d4d6e392019ec04ee34b09e5eec68794d
Extibax <extibax@gmail.com>
2019-05-05 20:37:29 -0500

    Some folders deleted

--- first_node_crud_mysql/src/App.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-/* Settings */
-app.set('port', process.env.PORT || 3000);
-
-app.listen(app.get('port'), () => {
-    console.log("Server Port 3000");
-});
--- node_app/src/index.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function() {
-    console.log('Me tarde 2s');
-}, 2000);
-
-console.log('Ejecutando');
