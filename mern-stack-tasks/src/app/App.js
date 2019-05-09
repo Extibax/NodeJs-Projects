3196b28b1fd40e038db3416aa31f18f635cb6139
Extibax <extibax@gmail.com>
2019-05-09 13:22:57 -0500

    wip

--- mern-stack-tasks/src/app/App.js
+++ mern-stack-tasks/src/app/App.js
@@ -6,15 +6,80 @@ class App extends Component {
         super();
         this.state = {
             title: '',
-            description: ''
+            description: '',
+            tasks: []
         };
         this.addTask = this.addTask.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
-    addTask(e) {
+    async addTask(e) {
         e.preventDefault();
-        console.log(this.state);
+        const result = await fetch('/api/tasks', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(await result.json());
+        M.toast({html: 'Task saved'});
+        this.setState({
+            title: '',
+            description: ''
+        });
+        this.fetchTasks();
+    }
+
+    componentDidMount() {
+        this.fetchTasks();
+    }
+
+    async fetchTasks() {
+        const result = await fetch('/api/tasks');
+        const data = await result.json();
+        this.setState({
+            tasks: data
+        });
+        console.log(result);
+        console.log(this.state.tasks);
+        /* fetch('/api/tasks')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    tasks: data
+                });
+                console.log(this.state.tasks);
+            }); */
+    }
+
+    async deleteTask(id) {
+        console.log('Deleting: ' + id);
+        fetch(`/api/tasks/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            M.toast({html: 'Task deleted'});
+        });
+        /* console.log('Deleting: ' + id);
+        const result = await fetch(`/api/tasks/${id}`, {
+            method: 'DELETE',
+            headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json' 
+            }
+        });
+        const res = await result.json();
+        console.log(res);
+        M.toast({html: 'Task deleted'});
+        this.fetchTasks(); */
     }
     
     handleChange(e) {
@@ -36,21 +101,21 @@ class App extends Component {
 
                 <div className="container">
                     <div className="row">
-                        <div className="col s5">
+                        <div className="col s4">
                             <div className="card">
                                 <div className="card-content">
                                     <form onSubmit={this.addTask}>
                                         <div className="row">
                                             <div>
                                                 <div>
-                                                    <input onChange={this.handleChange} type="text" name="title" placeholder="Task Title" />
+                                                    <input onChange={this.handleChange} type="text" name="title" placeholder="Task Title" value={this.state.title} />
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div>
                                                 <div>
-                                                    <textarea onChange={this.handleChange} className="materialize-textarea" name="description" placeholder="Task Description"></textarea>
+                                                    <textarea onChange={this.handleChange} className="materialize-textarea" name="description" placeholder="Task Description" value={this.state.description}></textarea>
                                                 </div>
                                             </div>
                                         </div>
@@ -65,8 +130,41 @@ class App extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="col s7">
-                            
+                        <div className="col s8">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.tasks.map((task) => {
+                                            return (
+                                                <tr key={task._id}>
+                                                    <td>{task.title}</td>
+                                                    <td>{task.description}</td>
+                                                    <td>
+                                                        <button className="btn light-blue darken-4">
+                                                            <i className="material-icons">
+                                                                edit
+                                                            </i>
+                                                        </button>
+                                                    </td>
+                                                    <td>
+                                                        <button className="btn light-blue darken-4" onClick={() => this.deleteTask(task._id)}>
+                                                            <i className="material-icons">
+                                                                    delete
+                                                            </i>
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
